3c51687d6062d28f3ec026dbb5600890913bfff9
Jacty <saej3101@usherbrooke.ca>
2021-10-27 11:46:55 -0400

    Fini recherche par theme et debut recherche avancee

---
+++ distribue/proxy/html/wiki/shared/app1.js
@@ -0,0 +1,56 @@
+function createSideNavLink(theme) {
+    return '<a onclick="getArticleByTheme(\'' + theme.id_thematique + '\')" href="#' + theme.id_thematique + '">' + theme.nom_thematique + '</a>'
+
+}
+
+function loadThemesOnSideNav() {
+    var sideNav = document.getElementById("sidenav");
+    axios.get("http://localhost:8888/api/thematique")
+        .then(function (response) {
+            console.log("api/thematique");
+            console.log(response.status);
+
+            var thematiques = response.data;
+            thematiques.forEach(function(thematique) {
+                var htmlLink = createSideNavLink(thematique);
+                sideNav.innerHTML += (htmlLink);
+            });
+        })
+        .catch(function (error) {
+            console.log('refreshing');
+            keycloak.updateToken(5).then(function () {
+                console.log('Token refreshed');
+            }).catch(function () {
+                console.log('Failed to refresh token');
+            })
+            alert(error);
+        });
+}
+
+function getArticleByTheme(id_theme) {
+    axios.get("http://localhost:8888/api/wikiByThematique/" + id_theme)
+        .then(function (response) {
+            console.log("Select by thematique");
+            console.log(response.status);
+
+            const articles = response.data;
+            const list_articles = document.getElementById('liste_article');
+            list_articles.innerHTML = "";
+
+            // Mettre data trouvé dans la liste
+            articles.forEach(article => {
+                var htmlLink = createArticleLink(article);
+                liste_article.innerHTML += (htmlLink);
+                console.log(article.nom_article);
+            })
+        })
+        .catch(function (error) {
+            console.log('refreshing');
+            keycloak.updateToken(5).then(function () {
+                console.log('Token refreshed');
+            }).catch(function () {
+                console.log('Failed to refresh token');
+            })
+            alert(error);
+        });
+}

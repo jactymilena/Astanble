aa430d6900c8ac3303e8b31277b418db9f9bf8b3
Yan <yanharc@gmail.com>
2021-10-29 11:15:11 -0400

    app1 -> app

--- distribue/proxy/html/wiki/shared/app.js
+++ distribue/proxy/html/wiki/shared/app.js
@@ -1,5 +1,5 @@
 function createSideNavLink(theme) {
-    return '<a onclick="alert(\'Thematique: ' + theme.nom_thematique + '\')" href="#' + theme.id_thematique + '">' + theme.nom_thematique + '</a>'
+    return '<a onclick="getArticleByTheme(\'' + theme.id_thematique + '\')" href="#' + theme.id_thematique + '">' + theme.nom_thematique + '</a> <br>'
 }
 
 function loadThemesOnSideNav() {
@@ -15,6 +15,37 @@ function loadThemesOnSideNav() {
                 sideNav.innerHTML += (htmlLink);
             });
             choixThemes(thematiques, "choix-theme");
+        })
+        .catch(function (error) {
+            console.log('refreshing');
+            keycloak.updateToken(5).then(function () {
+                console.log('Token refreshed');
+            }).catch(function () {
+                console.log('Failed to refresh token');
+            })
+            alert(error);
+        });
+}
+
+function getArticleByTheme(id_theme) {
+    axios.get("http://localhost:8888/api/wikiByThematique/" + id_theme)
+        .then(function (response) {
+            console.log("Select by thematique");
+            console.log(response.status);
+
+            const articles = response.data;
+            const list_articles = document.getElementById('liste_article');
+            list_articles.innerHTML = "";
+
+            document.getElementById('searchCloseButton').style.visibility = 'hidden';
+            document.getElementById('champSearch').value = "";
+
+            // Mettre data trouvé dans la liste
+            articles.forEach(article => {
+                var htmlLink = createArticleLink(article);
+                liste_article.innerHTML += (htmlLink);
+                console.log(article.nom_article);
+            })
         })
         .catch(function (error) {
             console.log('refreshing');
